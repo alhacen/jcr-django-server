7faccb832afc217c906081721a155372b912a67c
Faisal Manzer <faisal_manzer@yahoo.in>
2019-11-01 02:51:21 +0530

    corrected upcjmi bug

--- __jcr__/hosts.py
+++ __jcr__/hosts.py
@@ -4,7 +4,7 @@ from __jcr__.secret import SECRET
 HOST = SECRET['SERVER']['HOSTS']
 
 host_patterns = patterns('',
-                         host(HOST['API'], 'upcjmi.urls.api', name='api'),
-                         host(HOST['ADMIN'], 'upcjmi.urls.admin', name='admin'),
-                         host('dev', 'upcjmi.urls.dev', name='dev')
+                         host(HOST['API'], '__jcr__.urls.api', name='api'),
+                         host(HOST['ADMIN'], '__jcr__.urls.admin', name='admin'),
+                         host('dev', '__jcr__.urls.dev', name='dev')
                          )
